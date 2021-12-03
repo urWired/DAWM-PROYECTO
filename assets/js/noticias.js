96bcec24f2e76d8154be7188c4c6f67e017d9c23
Noelia Alejandra Intriago Sánchez <noinsanc@espol.edu.ec>
2021-12-02 21:47:23 -0500

    Cambios en noticias con uso de json

--- assets/js/noticias.js
+++ assets/js/noticias.js
@@ -1,3 +1,33 @@
+const peticion = () => {
+  fetch("assets/data/noticias.json")
+  .then(response => response.json())
+  .then(data => {
+    let noticias = data.noticias;
+    console.log(noticias);
+    for(let i=0; i < noticias.length; i++){
+      let titulo = noticias[i].titulo;
+      let fecha = noticias[i].fecha;
+      let descripcion = noticias[i].contenido;
+      let plantilla = ` 
+      <div class="col">
+        <div class="card h-100"">
+          <div class="card-body">
+            <h5 class="card-title">${titulo}</h5>
+            <p class="card-date">${fecha}</p>
+            <div class="separator separator-danger">∎</div>
+            <p class="card-text">${descripcion}</p>
+          </div>
+        </div>
+      </div>
+      `;
+
+      document.getElementById("news-container").innerHTML += plantilla;
+    }
+  })
+  .catch(console.error);
+}
+
+
 const buscador = () => {
   let input = document.getElementById("search-input");
 
@@ -22,4 +52,5 @@ const buscador = () => {
   })
 }
 
+peticion();
 buscador();
