cb9dfe17e31adb34e98eb018cdb2aaa3ea0fd5a8
DGV <davidgarciavin@outlook.com>
2022-01-18 00:53:36 -0500

    componentes quienesSomos, noticias y login

--- asvolhClient/src/app/app.module.ts
+++ asvolhClient/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuienesSomosComponent,
+    NoticiasComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
