b0c96c133447bf6eb118610a01019b5783eb8eb0
DGV <davidgarciavin@outlook.com>
2022-01-19 21:31:33 -0500

    rutas y componentes nuevos

--- asvolhClient/src/app/app-routing.module.ts
+++ asvolhClient/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EquipoDesarrolloComponent } from './equipo-desarrollo/equipo-desarrollo.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NoticiasComponent } from './noticias/noticias.component';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 
 const routes: Routes = [
   { path: 'noticias', component: NoticiasComponent },
-  { path: '', component: InicioComponent}
+  { path: '', component: InicioComponent},
+  { path: 'equipo', component: EquipoDesarrolloComponent},
+  { path: 'quienessomos', component: QuienesSomosComponent}
+  
 ];
 
 @NgModule({
