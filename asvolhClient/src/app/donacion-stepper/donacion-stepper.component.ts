dcedb3ceeb15fbb784ed23bb452e6d32a7714b0c
Marco Del Rosario <marsdel@espol.edu.ec>
2022-01-31 14:46:45 -0500

    implementación de registro de donación

--- asvolhClient/src/app/donacion-stepper/donacion-stepper.component.ts
+++ asvolhClient/src/app/donacion-stepper/donacion-stepper.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+interface TiposPago {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-donacion-stepper',
   templateUrl: './donacion-stepper.component.html',
@@ -9,15 +14,38 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class DonacionStepperComponent implements OnInit {
 
   profileFormGroup: FormGroup;
-  addressFormGroup: FormGroup;
+  contactoFormGroup: FormGroup;
+  donacionFormGroup: FormGroup;
+  detallesFormGroup: FormGroup;
+
+  selectedValue: string;
+  tipo_pago: TiposPago[];
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.tipo_pago = [
+      {value: '1', viewValue: 'PayPal'},
+      {value: '2', viewValue: 'Transferencia Bancaria'},
+      {value: '3', viewValue: 'Diners, Visa, MasterCard, Discover & Tarjetas de Débito (Solo Tarjetas Nacionales)'},
+    ];
+
     this.profileFormGroup = this.fb.group({
-      name: [" ", Validators.required]
+      cedula: ["", Validators.required],
+      nombres: ["", Validators.required]
     });
-    this.addressFormGroup = this.fb.group({
-      address: [" ", Validators.required]
+    this.contactoFormGroup = this.fb.group({
+      celular: ["", Validators.required],
+      correo: ["", Validators.required]
+    });
+    this.donacionFormGroup = this.fb.group({
+      tipo_pago: ["", Validators.required],
+      cuenta: ["", Validators.required],
+      monto: ["", Validators.required],
+    });
+
+    this.detallesFormGroup = this.fb.group({
+      detalles: ["", Validators.required]
     });
   }
 
